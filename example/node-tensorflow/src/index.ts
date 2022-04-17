69587de89b93c4c5454532152188a6ad7fed0a6c
Ömür Yıldırım <omuryildirim@ymail.com>
2022-04-17 16:49:12 +0200

    import local module by path

--- example/node-tensorflow/src/index.ts
+++ example/node-tensorflow/src/index.ts
@@ -1,13 +1,9 @@
 import * as tf from '@tensorflow/tfjs';
+import {BeforeEvening, StateUpdate} from '../../../src';
 
-import { BeforeEvening } from '../../../build/main';
-
-import { Memory } from './memory';
-import { SaveablePolicyNetwork } from './policy-network';
-import {
-  ReinforcementLearningModel,
-  StateUpdate
-} from './reinforcement-learning.model';
+import {Memory} from './memory';
+import {SaveablePolicyNetwork} from './policy-network';
+import {ReinforcementLearningModel,} from './reinforcement-learning.model';
 import {
   ActionKeyEventMapper,
   ActionKeyToEventName
