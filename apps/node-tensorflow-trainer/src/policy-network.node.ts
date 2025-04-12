b2776fabe358b3c1633464aff1630d0e0233bb36
Ömür Yıldırım <omuryildirim@ymail.com>
2025-04-12 16:01:21 +0200

    feat: introduce node level model creation and model save functions
    this will prevent using tsjs for model creation, hence when saving file locally it will not throw cannot find save handler error

--- apps/node-tensorflow-trainer/src/policy-network.node.ts
+++ apps/node-tensorflow-trainer/src/policy-network.node.ts
@@ -1,8 +1,50 @@
 import * as tf from "@tensorflow/tfjs-node";
 
-import { SaveablePolicyNetwork } from "@before-evening/shared";
+import {
+	ACTIVATION,
+	LOSS,
+	NUMBER_OF_ACTIONS,
+	NUMBER_OF_STATES,
+	OPTIMIZER,
+	SaveablePolicyNetwork,
+	type SaveablePolicyNetworkParams,
+} from "@before-evening/shared";
 
 export class SaveableNodePolicyNetwork extends SaveablePolicyNetwork {
+	constructor({
+		hiddenLayerSizesOrModel,
+		maxStepsPerGame,
+		modelName,
+	}: SaveablePolicyNetworkParams) {
+		let model = hiddenLayerSizesOrModel;
+
+		if (Number.isInteger(model)) {
+			model = tf.sequential();
+			[hiddenLayerSizesOrModel as number].forEach((hiddenLayerSize, i) => {
+				(model as tf.Sequential).add(
+					tf.layers.dense({
+						units: hiddenLayerSize,
+						activation: ACTIVATION,
+						// `inputShape` is required only for the first layer.
+						inputShape: i === 0 ? [NUMBER_OF_STATES] : undefined,
+					}),
+				);
+			});
+			(model as tf.Sequential).add(
+				tf.layers.dense({ units: NUMBER_OF_ACTIONS }),
+			);
+
+			model.summary();
+			model.compile({ optimizer: OPTIMIZER, loss: LOSS });
+		}
+
+		super({
+			hiddenLayerSizesOrModel: model,
+			maxStepsPerGame,
+			modelName,
+		});
+	}
+
 	/**
 	 * Load the model fom IndexedDB.
 	 *
@@ -28,5 +70,9 @@ export class SaveableNodePolicyNetwork extends SaveablePolicyNetwork {
 			});
 		}
 		throw new Error(`Cannot find model at ${modelName}.`);
+	}
+
+	async saveModel(): Promise<ReturnType<typeof this.model.network.save>> {
+		return await this.model.network.save(`file://${this.modelName}`);
 	}
 }
