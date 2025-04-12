b7167135033742c472615c6227ce29e9c461ba85
Ömür Yıldırım <omuryildirim@ymail.com>
2025-04-12 16:01:53 +0200

    feat: move certain values to constants

--- apps/shared/src/constants.ts
+++ apps/shared/src/constants.ts
@@ -2,3 +2,5 @@ export const MODEL_VERSION = "before-evening-v6";
 export const OPTIMIZER = "adam";
 export const LOSS = "meanSquaredError";
 export const ACTIVATION = "relu";
+export const NUMBER_OF_ACTIONS = 8;
+export const NUMBER_OF_STATES = 7;
